f8bf6d66db8b571ad9dd7e58f6d9ecadc3cc1245
ponomarovs87 <ponomarovs87@gmail.com>
2023-05-17 18:52:48 +0200

    edit syntaxys add html/css/3.js/4.js start

---
+++ script/main.js
@@ -0,0 +1,12 @@
+// import 
+
+// Импортируем функции из файла 3.js
+import { addTask, displayErrorMessage, resetErrorStyles } from './3.js';
+// Импортируем функции из файла 4.js
+import './4.js';
+
+// Используем функции resetErrorStyles и addTask в вашем коде
+resetErrorStyles();
+addTask();
+
+
